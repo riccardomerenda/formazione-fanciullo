bd1a04b42413ca535d6c7fd74d0b0a855b7b3a1b
Riccardo Merenda <riccardomerenda@gmail.com>
2015-07-13 00:14:28 +0200

    aggiunto esercizio CiaoMondoConLogin

--- android/CambiaSfondo/app/src/main/java/it/cambiasfondo/MainActivity.java
+++ android/CambiaSfondo/app/src/main/java/it/cambiasfondo/MainActivity.java
@@ -62,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
                 break;
             }
             case "Rosso":{
-                //todo
+                //todo9
                 break;
             }
             default:{
---
+++ android/CiaoMondoConLogin/app/src/main/java/com/example/ciaomondoconlogin/LoginActivity.java
@@ -0,0 +1,45 @@
+package com.example.ciaomondoconlogin;
+
+import java.util.Random;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.EditText;
+
+public class LoginActivity extends Activity {
+
+    private EditText uname;
+    private EditText passwd;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        uname = (EditText) findViewById(R.id.username_edittext);
+        passwd = (EditText) findViewById(R.id.password_edittext);
+    }
+
+    public void eseguiLogin(View v) {
+        if (checkPassword(uname.getText(), passwd.getText())) {
+
+            // Creiamo un intent esplicito per avviare MainActivity
+            Intent helloAndroidIntent = new Intent(LoginActivity.this,
+                    MainActivity.class);
+
+            // Use the Intent to start the HelloAndroid Activity
+            startActivity(helloAndroidIntent);
+        } else {
+            uname.setText("");
+            passwd.setText("");
+        }
+    }
+
+    private boolean checkPassword(Editable uname, Editable passwd) {
+        // Fingiamo di estrarre il testo e verificare le credenziali
+        return new Random().nextBoolean();
+    }
+}
