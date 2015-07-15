9ef0fdf577f3bd11dceaec30ff196610b7c8c840
Davide Agostini <dsagostini@gmail.com>
2015-07-15 10:41:23 +0200

    esempio su linear e relative layout

--- android/CambiaSfondoAvanzato/app/src/main/java/it/cambiasfondoavanzato/MainActivity.java
+++ android/CambiaSfondoAvanzato/app/src/main/java/it/cambiasfondoavanzato/MainActivity.java
@@ -1,53 +1,92 @@
-package it.cambiasfondoavanzato;
+    package it.cambiasfondoavanzato;
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.RelativeLayout;
+    import android.os.Bundle;
+    import android.support.v7.app.AppCompatActivity;
+    import android.util.Log;
+    import android.view.View;
+    import android.widget.RelativeLayout;
 
-/**
- * Questa applicazione e' una versione avanzata del cambia sfondo visto precedentemente.
- * Essa non fa altro che mostrare una serie di bottoni colorati dove al click dell'utente
- * viene cambiato il colore di sfondo.
- * Lo scopo è quello di accedere alle risorse dichiarate, come bottone, layout, ecc.. e ripassare
- * anche i costrutti di base di java.
- *
- */
-public class MainActivity extends AppCompatActivity {
+    /**
+     * Questa applicazione e' una versione avanzata del cambia sfondo visto precedentemente.
+     * Essa non fa altro che mostrare una serie di bottoni colorati dove al click dell'utente
+     * viene cambiato il colore di sfondo.
+     * Lo scopo è quello di accedere alle risorse dichiarate, come bottone, layout, ecc.. e ripassare
+     * anche i costrutti di base di java.
+     *
+     */
+    public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+        private final String TAG = "CambiaSfondoAvanzato";
 
-    public void cambiaSfondo(View v) {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+        }
 
-        RelativeLayout layout = (RelativeLayout) findViewById(R.id.sfondoActvity);
+        public void cambiaSfondo(View v) {
 
-        /**
-         * controllo qual è l'id del bottone premuto dall'utente
-         * e a seconda dei casi setto il colore di background del layout
-         */
-        switch(v.getId()) {
-            case R.id.buttonGiallo:
-                layout.setBackgroundColor(getResources().getColor(R.color.Giallo));
-                break;
-            case R.id.buttonRosso:
-                layout.setBackgroundColor(getResources().getColor(R.color.Rosso));
-                break;
-            case R.id.buttonVerde:
-                layout.setBackgroundColor(getResources().getColor(R.color.Verde));
-                break;
-            case R.id.buttonBlue:
-                layout.setBackgroundColor(getResources().getColor(R.color.Blue));
-                break;
-            case R.id.buttonRosa:
-                layout.setBackgroundColor(getResources().getColor(R.color.Rosa));
-                break;
-            case R.id.buttonArancio:
-                layout.setBackgroundColor(getResources().getColor(R.color.Arancio));
-                break;
+            RelativeLayout layout = (RelativeLayout) findViewById(R.id.sfondoActvity);
+
+            /**
+             * controllo qual è l'id del bottone premuto dall'utente
+             * e a seconda dei casi setto il colore di background del layout
+             */
+            switch(v.getId()) {
+                case R.id.buttonGiallo:
+                    layout.setBackgroundColor(getResources().getColor(R.color.Giallo));
+                    break;
+                case R.id.buttonRosso:
+                    layout.setBackgroundColor(getResources().getColor(R.color.Rosso));
+                    break;
+                case R.id.buttonVerde:
+                    layout.setBackgroundColor(getResources().getColor(R.color.Verde));
+                    break;
+                case R.id.buttonBlue:
+                    layout.setBackgroundColor(getResources().getColor(R.color.Blue));
+                    break;
+                case R.id.buttonRosa:
+                    layout.setBackgroundColor(getResources().getColor(R.color.Rosa));
+                    break;
+                case R.id.buttonArancio:
+                    layout.setBackgroundColor(getResources().getColor(R.color.Arancio));
+                    break;
+            }
+        }
+
+        @Override
+        protected void onStart(){
+            super.onStart();
+            Log.d(TAG, "Activity visibile e in procinto di essere avviata");
+        }
+
+        @Override
+        protected void onRestart(){
+            super.onRestart();
+            Log.d(TAG,"Activity è visibile e in procinto di essere riavviata");
+        }
+
+        @Override
+        protected void onResume(){
+            super.onResume();
+            Log.d(TAG, "Activity <<riesumata>>");
+        }
+
+        @Override
+        protected void onPause(){
+            super.onPause();
+            Log.d(TAG, "Un'altra Activity ha il focus");
+        }
+
+        @Override
+        protected void onStop(){
+            super.onStop();
+            Log.d(TAG,"Activity non più visibile");
+        }
+
+        @Override
+        protected void onDestroy(){
+            super.onDestroy();
+            Log.d(TAG, "Activity viene cancellata");
         }
     }
-}
---
+++ android/LinearAndRelativeLayout/app/src/main/java/it/linearandrelativelayout/MainActivity.java
@@ -0,0 +1,64 @@
+package it.linearandrelativelayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+/**
+ * questo esempio mostra la differenza tra il linear layout e
+ * il relative layout.
+ *
+ * Il LinearLayout è una view che allinea tutti i figli in una singola
+ * direzione, verticale o orizzontale. La direzione si specifica tramite android:orientation.
+ * Una caratteristica importante di questo layout e' che supporta l'attributo peso.
+ * Il peso indica quanto spazio quella view deve occupare sullo schermo.
+ *
+ * Il RelativeLayout è una view che posiziona tutti i figli della view in posizione relativa.
+ * La posizione di ciascuna view pue' essere specificata come relativa ad un elemento della view
+ * (come ad esempio a sinistra di un elemento o sotto) oppure in posizione relativa al RelativeLayout
+ * (come ad esempio allineato al fondo, sinistra o centrale).
+ *
+ * Vengono introdotti anche i Toast. Un toast fornisce un semplice feedback su un'operazione all'interno
+ * di un piccolo popup. Esso riempie solo lo spazio richiesto per mostrare il messaggio.
+ * L'attivita' corrente rimane visibile e intereattiva.
+ *
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        /**
+         * questo setta il layout di tipo linear layout
+         */
+        //setContentView(R.layout.activity_main);
+
+        /**
+         * decommentando la riga seguente il layout utilizzato sara'
+         * quello di tipo relative layout
+         */
+        setContentView(R.layout.relative_layout);
+    }
+
+    /**
+     * metodo clicca bottone che ci mostra un messaggio di avviso sul bottone premuto
+     * @param v
+     */
+    public void clickBottone(View v) {
+
+        switch (v.getId()) {
+            case R.id.button1:
+                Toast.makeText(getApplicationContext(), "Premuto Bottone 1", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button2:
+                Toast.makeText(getApplicationContext(), "Premuto Bottone 2", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button3:
+                Toast.makeText(getApplicationContext(), "Premuto Bottone 3", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
